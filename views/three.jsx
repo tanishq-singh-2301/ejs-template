30577ea7124bd7339ccaba299188ea27cda68086
Tanishq Singh <>
2021-10-16 23:38:53 +0530

    Introduced threejs

--- views/three.jsx
+++ views/three.jsx
@@ -1,58 +1,19 @@
 import React, { useEffect } from 'react';
 import '../assets/css/three.scss';
 import Header from './components/header.jsx';
+import Experience from '../models/Experience';
 import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
-class Experience {
+class World extends Experience {
     constructor({ targetElement }) {
-        this.targetElement = document.getElementById(targetElement);
+        super({ targetElement });
 
-        this.setScene();
-        this.setCamera();
-        this.setRenderer();
-
-        this.update();
-        window.addEventListener('resize', () => this.resize());
+        this.setInstance();
     };
 
-    setScene() {
-        this.scene = new THREE.Scene();
-        this.scene.background = new THREE.Color(0x000000);
-
-        this.world = new World({ scene: this.scene });
-    };
-
-    setCamera() {
-        this.camera = new Camera({ targetElement: this.targetElement, scene: this.scene });
-    };
-
-    setRenderer() {
-        this.renderer = new Renderer({ targetElement: this.targetElement, scene: this.scene, camera: this.camera.instance });
-    };
-
-    resize() {
-        this.renderer ? this.renderer.resize() : null;
-        this.camera ? this.camera.resize() : null;
-    };
-
-    update() {
-        this.camera ? this.camera.update() : null;
-        this.renderer ? this.renderer.update() : null;
-
-        window.requestAnimationFrame(() => this.update());
-    };
-};
-
-class World {
-    constructor({ scene }) {
-        this.scene = scene;
-        this.setDummy();
-    };
-
-    setDummy() {
+    setInstance() {
         this.box = new THREE.Mesh(
-            new THREE.BoxGeometry(5, 5, 5),
+            new THREE.SphereBufferGeometry(4, 20, 20),
             new THREE.MeshBasicMaterial({
                 color: 'dimgray',
                 wireframe: true
@@ -63,80 +24,9 @@ class World {
     };
 };
 
-class Camera {
-    constructor({ targetElement, scene }) {
-        this.targetElement = targetElement;
-        this.scene = scene;
-
-        this.setInstance();
-    };
-
-    setInstance() {
-        this.instance = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight);
-        this.instance.position.set(4, 10, 13);
-        this.scene.add(this.instance);
-
-        this.orbitControl = new OrbitControls(this.instance, this.targetElement)
-        this.orbitControl.enableDamping = true;
-    };
-
-    resize() {
-        this.instance.aspect = window.innerWidth / window.innerHeight;
-    };
-
-    update() {
-        this.instance.updateProjectionMatrix();
-        this.orbitControl.update();
-    };
-};
-
-class Renderer {
-    constructor({ targetElement, scene, camera }) {
-        this.targetElement = targetElement;
-        this.scene = scene;
-        this.camera = camera;
-
-        this.setInstance();
-    };
-
-    setInstance() {
-        this.instance = new THREE.WebGL1Renderer({
-            canvas: this.targetElement,
-            alpha: false,
-            antialias: true
-        });
-        this.instance.setSize(window.innerWidth, window.innerHeight);
-        this.instance.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-
-        this.instance.physicallyCorrectLights = true;
-        this.instance.shadowMap.enabled = true;
-        this.instance.shadowMap.type = THREE.PCFSoftShadowMap;
-        // this.instance.gammaOutPut = true
-        // this.instance.outputEncoding = THREE.sRGBEncoding
-        // this.instance.toneMapping = THREE.ReinhardToneMapping
-        // this.instance.toneMapping = THREE.ReinhardToneMapping
-        // this.instance.toneMappingExposure = 1.3
-        this.instance.render(this.scene, this.camera);
-    };
-
-    resize() {
-        this.instance.setSize(window.innerWidth, window.innerHeight);
-        this.instance.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-    };
-
-    update() {
-        this.instance.render(this.scene, this.camera);
-    };
-
-    destroy() {
-        this.instance.renderLists.dispose();
-        this.instance.dispose();
-    };
-};
-
 const Three = () => {
     useEffect(() => {
-        const experience = new Experience({ targetElement: 'three-canvas' });
+        const world = new World({ targetElement: 'three-canvas' });
     }, []);
 
     return (
